99b792cc667960f2e762d7c9ed933fab5f9a309c
Tae-kyeom <kimtkyeom@gmail.com>
2022-10-09 14:17:42 +0900

    Add backoffice healthz check skipper

--- go/server/backoffice/backoffice_middleware.go
+++ go/server/backoffice/backoffice_middleware.go
@@ -5,16 +5,26 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+var skipSet = map[string]struct{}{
+	"/healthz": {},
+}
+
 type sessionMiddleware struct {
 	secretKey string
 }
 
 func (s sessionMiddleware) Get() echo.MiddlewareFunc {
 	return middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
+		Skipper: s.skipper,
 		Validator: func(auth string, c echo.Context) (bool, error) {
 			return auth == s.secretKey, nil
 		},
 	})
+}
+
+func (s sessionMiddleware) skipper(c echo.Context) bool {
+	_, ok := skipSet[c.Path()]
+	return ok
 }
 
 func NewSessionMiddleware(secretKey string) sessionMiddleware {
