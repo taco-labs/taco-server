5059f7b09e93f82958b23bb26e11bb2da2525eef
Tae-kyeom <kimtkyeom@gmail.com>
2023-01-26 23:12:33 +0900

    Remove min price adjustment in mock payment

--- go/domain/entity/checkers.go
+++ go/domain/entity/checkers.go
@@ -15,10 +15,6 @@ func CheckPaymentForTaxiCallRequest(taxiCallRequest *TaxiCallRequest, userPaymen
 		if taxiCallRequest.RequestMaxAdditionalPrice > SignupPromotionMaxAdditionalPrice {
 			return value.ErrInvalidPromotionPaymentUsage
 		}
-		// TODO (taekyeom) promotion일 경우 min additional price를 최소 5000원으로 설정
-		if taxiCallRequest.RequestMinAdditionalPrice < 5000 {
-			taxiCallRequest.RequestMinAdditionalPrice = 5000
-		}
 	}
 	return nil
 }
